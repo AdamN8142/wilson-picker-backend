0715cb30d1741054aabc2d37cdb501e6b9653b4e
Saad Baradan <saadbaradan@gmail.com>
2019-07-11 17:57:41 -0600

    Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,3 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'pg',
@@ -11,16 +9,32 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
-},
-test: {
-  client: "pg",
-  connection: "postgres://localhost/projects_test",
-  migrations: {
-    directory: "./db/migrations"
   },
-  seeds: {
-    directory: "./db/seeds/dev"
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
   },
-  useNullAsDefault: true,
-}
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/projects_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
+  
 };
+
+
